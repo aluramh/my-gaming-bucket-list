9602e9d26196bf93fe47a34ffbd5e9fb9c04f2af
Alex R <aluramh@gmail.com>
2022-10-29 12:58:50 -0700

    feat: index

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,85 +1,8 @@
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
-      <div className="max-w-3xl mx-auto pb-28">
-        <div className="text-6xl mt-4 mb-3">My Gaming Bucket List</div>
-        <p>This is my page</p>
-
-        <div className="p-8 bg-amber-300">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              1
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              2
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              3
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              4
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              5
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              6
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              7
-            </div>
-            <div className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">
-              8
-            </div>
-          </div>
-        </div>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at
-          est urna. Sed commodo mauris id sapien cursus ullamcorper. Nunc
-          commodo dui sapien, nec pharetra mi bibendum quis. Mauris justo nibh,
-          hendrerit sit amet nisi in, interdum finibus ante. Duis euismod justo
-          ac elit ultrices cursus. Aenean tristique ut arcu eget efficitur.
-          Suspendisse lacus dolor, varius eget sodales egestas, vestibulum id
-          metus. In non rutrum risus. In vitae sollicitudin felis. Donec id
-          convallis orci. Ut scelerisque fringilla mauris sit amet malesuada.
-          Quisque suscipit rhoncus nunc eu iaculis. Nunc vitae neque posuere,
-          scelerisque nunc sit amet, vehicula eros. Donec non mi sit amet tellus
-          tristique aliquet sed ac diam. Nullam aliquam venenatis lacus.
-          Curabitur aliquet ipsum enim, id venenatis massa auctor vestibulum.
-        </p>
-        <p>
-          Sed at odio sit amet nisl gravida suscipit et eu leo. Pellentesque
-          mauris nisl, tincidunt sed mattis vitae, pretium et nibh. Aenean
-          laoreet massa dolor, in dapibus lacus egestas eget. Nulla vitae ante
-          turpis. In enim turpis, dignissim at vulputate vel, hendrerit nec
-          erat. Integer nisl ipsum, aliquet semper odio in, venenatis posuere
-          justo. Vivamus orci sapien, malesuada sed mauris eu, venenatis dictum
-          libero. Ut eget convallis risus. Maecenas libero sapien, porta sed
-          arcu vitae, viverra volutpat lectus. Ut eleifend feugiat massa, nec
-          efficitur lorem pretium eu.
-        </p>
-        <p>
-          Sed ut odio et neque pulvinar porta. Morbi magna elit, consectetur
-          eget facilisis ac, gravida eget purus. Suspendisse potenti. Integer
-          eget ex quam. Phasellus a lectus risus. Mauris eget justo ac velit
-          vestibulum scelerisque. Vivamus sit amet gravida diam, vitae ultricies
-          sem. Maecenas vitae nunc finibus, condimentum eros eget, accumsan
-          nulla. Aenean felis sem, vestibulum sit amet hendrerit vel, feugiat eu
-          quam. Curabitur sem neque, molestie ac nibh auctor, placerat porta
-          turpis. Aenean nec risus quis orci blandit suscipit. Aenean tempor
-          vulputate mauris eget bibendum. Pellentesque tristique sodales turpis
-          sit amet facilisis.
-        </p>
-        <p>
-          Fusce tincidunt leo ac ornare laoreet. Aliquam in pretium urna.
-          Phasellus nec vehicula quam. Cras auctor odio magna, a ultricies risus
-          accumsan eget. Nam sodales eros nec est vestibulum efficitur. Nunc
-          facilisis, eros vitae porttitor pulvinar, tellus dolor mollis purus,
-          non volutpat nunc nunc condimentum justo. Nulla euismod augue sed mi
-          luctus bibendum. Duis at finibus sapien.
-        </p>
-      </div>
+    <div className="container max-w-3xl mx-auto pb-28 px-4">
+      <div className="text-6xl mt-4 mb-3">My Gaming Bucket List</div>
+      <p>This is my page</p>
     </div>
   )
 }
